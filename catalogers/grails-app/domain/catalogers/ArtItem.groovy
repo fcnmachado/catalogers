a7a5c3ea4b286dfd58c12891f511a8b06dc27658
Felipe Machado <fcnmachado@gmail.com>
2018-02-26 10:07:45 -0300

    creating first version of entity ArtItem

---
+++ catalogers/grails-app/domain/catalogers/ArtItem.groovy
@@ -0,0 +1,13 @@
+package catalogers
+
+class ArtItem {
+
+    String tittle
+    String author
+    String description
+    boolean published
+    Date date_created
+
+    static constraints = {
+    }
+}
